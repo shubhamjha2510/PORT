59a19a05a0210af2ef9b849b4d8eb9a6b1564186
shubhamjha2510 <30436356+shubhamjha2510@users.noreply.github.com>
2025-05-25 13:05:52 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["next-mdx-remote"],
+  allowedDevOrigins: ["chanhdai-macbook.local"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.chanhdai.com",
+        port: "",
+      },
+    ],
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           // Prevents MIME type sniffing, reducing the risk of malicious file uploads
+  //           key: "X-Content-Type-Options",
+  //           value: "nosniff",
+  //         },
+  //         {
+  //           // Protects against clickjacking attacks by preventing your site from being embedded in iframes.
+  //           key: "X-Frame-Options",
+  //           value: "DENY",
+  //         },
+  //         {
+  //           // Controls how much referrer information is included with requests, balancing security and functionality.
+  //           key: "Referrer-Policy",
+  //           value: "strict-origin-when-cross-origin",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+};
+
+export default nextConfig;
